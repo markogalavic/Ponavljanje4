addccb52d4160d38fe628be4b3821642b8a8dad1
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-11-17 10:49:23 +0100

    Add files via upload

---
+++ markoGalavicPonavljanje5/markoGalavicPonavljanje5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavicPonavljanje5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi cijenu bez pdv-a");
+            double bezpdva = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Unesite istu cijenu sa pdv-om");
+            
+            double sapdvom = bezpdva * 1.25;
+
+            Console.WriteLine("Cijena s PDV-om je :" + sapdvom);
+
+            Console.ReadKey();
+        }
+    }
+}
